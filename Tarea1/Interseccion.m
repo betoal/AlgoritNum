c9c74fca026971a60cafa3dd3aa4ca0fc1dd76b1
Alberto Acosta <beto.alop@gmail.com>
2018-08-18 01:34:45 +0000

    Final de Tarea

--- Tarea1/Interseccion.m
+++ Tarea1/Interseccion.m
@@ -1,16 +1,27 @@
 M = 100;
 N = 50;
 K = 200;
-Radio1 = 2;
-Radio2 = 2;
-cx1 = 1; cy1 = 0;
-cx2 = -1; cy2 = 0;
+R1 = 2; R2 = 2;
+c1 = [1 0]; c2 = [-1 0];
 A = zeros(M,N);
 B = zeros(M,N);
-for n = 1:N
-	k = K*N;
+Ans = zeros(M,N);
+for n = 1:N		%%Cantidad de columnas
+	k = K*N;	%%Cantidad de renglones
 	for m = 1:M
 		x = rand(k,1);
 		y = rand(k,1);
-		A(m,n) = ((x-cx1).*(x-cx1) + (y-cy1).*(y-cy1) <=1)/k;
-		
+		A(m,n) = ((x-c1(1)).*(x-c1(1)) + (y-c1(2)).*(y-c1(2)) <=(R1.*R1))/k;
+		B(m,n) = ((x-c2(1)).*(x-c2(1)) = (y-c2(2)).*(y-c2(2)) <=(R2.*R2))/k;
+		Ans(m,n) = sum(A(m,n).*B(m,n));
+	end
+end
+
+pos1 = [c1-R1, 2*R1 2*R1];
+pos2 = [c2-R2, 2*R2 2*R2];
+
+hold on
+plot(pos1)
+plot(pos2)
+plot(Ans','.')
+hold off
