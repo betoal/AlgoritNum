9de52da57adf614d03271fbd5c88e76c5332c039
Alberto Acosta <beto.alop@gmail.com>
2018-08-17 20:46:31 -0500

    Correccion error

--- Tarea1/Interseccion.m
+++ Tarea1/Interseccion.m
@@ -3,6 +3,7 @@ N = 50;
 K = 200;
 R1 = 2; R2 = 2;
 c1 = [1 0]; c2 = [-1 0];
+areaRect =(c1(1)-R1)*(2*(c1(2)+R1)); 
 A = zeros(M,N);
 B = zeros(M,N);
 Ans = zeros(M,N);
@@ -11,8 +12,8 @@ for n = 1:N		%%Cantidad de columnas
 	for m = 1:M
 		x = rand(k,1);
 		y = rand(k,1);
-		A(m,n) = ((x-c1(1)).*(x-c1(1)) + (y-c1(2)).*(y-c1(2)) <=(R1.*R1))/k;
-		B(m,n) = ((x-c2(1)).*(x-c2(1)) = (y-c2(2)).*(y-c2(2)) <=(R2.*R2))/k;
+		A(m,n) = (((x-c1(1)).*(x-c1(1)) + (y-c1(2)).*(y-c1(2)) <=(R1.*R1))/k)*areaRect;
+		B(m,n) = (((x-c2(1)).*(x-c2(1)) = (y-c2(2)).*(y-c2(2)) <=(R2.*R2))/k)*areaRect;
 		Ans(m,n) = sum(A(m,n).*B(m,n));
 	end
 end
