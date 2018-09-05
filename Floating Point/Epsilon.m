133563a5c6d2f8ade60c899e867625034dce6a23
ALBERTO ACOSTA LOPEZ <aacostal@itam.mx>
2018-09-05 10:27:37 -0500

    Floating point

---
+++ Floating Point/Epsilon.m
@@ -0,0 +1,16 @@
+% Obtener el epsilon de la máquina
+
+x = 1;
+t = 1; % Lo que se va a sumar
+k = 0; % Contador
+xa = 0; % x anterior
+
+while( x ~= xa)
+   k = k + 1;
+   t = t / 2;
+   xa = x;
+   x = x + t;
+   t
+   k
+end
+
